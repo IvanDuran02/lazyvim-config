00fd6e803c36aea27a306d360fe2101f00dbac81
Ivan Duran <iamivanduran02@gmail.com>
2024-01-06 21:19:20 -0500

    added cmp for code completion

--- lua/popo/lazy.lua
+++ lua/popo/lazy.lua
@@ -57,6 +57,13 @@ local lazy_plugins = {
 	dependencies = {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
 	},
 	config = function()
 		require("mason").setup()
@@ -70,7 +77,6 @@ local lazy_plugins = {
 				"golangci_lint_ls",
 				"html",
 				"marksman",
-				"pyre",
 				"tailwindcss",
 			},
 			handlers = {
@@ -81,6 +87,29 @@ local lazy_plugins = {
 				end,
 			}
 		})
+		-- Auto complete stuff
+		local cmp = require('cmp')
+		local cmp_select = {behavior = cmp.SelectBehavior.Select}
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require('luasnip').lsp_expand(args.body) -- for luasnip.
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+				['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+				['<C-y>'] = cmp.mapping.confirm({ select = true}),
+				['<C-Space>'] = cmp.mapping.complete(),
+			}),
+			sources = cmp.config.sources({
+				{ name = 'nvim_lsp' },
+				{ name = 'luasnip' },
+			},	{
+				{ name = 'buffer' },
+			})
+		})
+
 	end
     }
 }
