3cad9d312bb199f498cba20e5f0b8cd64771b289
Ivan Duran <iamivanduran02@gmail.com>
2025-01-15 13:10:50 -0500

    updated tsserver -> ts_ls

--- lua/popo/formatting.lua
+++ lua/popo/formatting.lua
@@ -1,93 +1,92 @@
 return {
-    {
-        { "WhoIsSethDaniel/mason-tool-installer.nvim"
-        },
-        {
-            'stevearc/conform.nvim',
-            config = function()
-                require("mason-tool-installer").setup({
-                    ensure_installed = {
-                        "prettierd",    -- Example formatter
-                        "stylua",       -- Another formatter
-                        "black",        -- Python formatter
-                    },
-                    auto_update = true, -- Auto-update tools when opening Neovim
-                })
-                require("conform").setup({
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        pattern = "*",
-                        callback = function(args)
-                            require("conform").format({ bufnr = args.buf })
-                        end,
-                    }),
-                    format_on_save = {
-                        -- These options will be passed to conform.format()
-                        timeout_ms = 500,
-                        lsp_fallback = true,
-                    },
-                    formatters_by_ft = {
-                        lua = { "stylua" },
-                        -- Conform will run multiple formatters sequentially
-                        python = { "isort", "black" },
-                        -- Use a sub-list to run only the first available formatter
-                        javascript = { { "prettierd", "prettier" } },
-                        go = { "goimports", "gofmt" },
-                    },
-                })
-            end
-        }
-    },
-    {
-        "windwp/nvim-ts-autotag",
-        lazy = false,
-        config = function()
-            require 'nvim-ts-autotag'.setup()
-        end,
-    },
-    {
-        'windwp/nvim-autopairs',
-        event = "InsertEnter",
-        opts = {}
-    },
-    {
-        "abecodes/tabout.nvim",
-        opts = {}
-    },
-    {
-        "folke/trouble.nvim",
-        opts = {}, -- for default options, refer to the configuration section for custom setup.
-        cmd = "Trouble",
-        keys = {
-            {
-                "<leader>xx",
-                "<cmd>Trouble diagnostics toggle<cr>",
-                desc = "Diagnostics (Trouble)",
-            },
-            {
-                "<leader>xX",
-                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-                desc = "Buffer Diagnostics (Trouble)",
-            },
-            {
-                "<leader>cs",
-                "<cmd>Trouble symbols toggle focus=false<cr>",
-                desc = "Symbols (Trouble)",
-            },
-            {
-                "<leader>cl",
-                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-                desc = "LSP Definitions / references / ... (Trouble)",
-            },
-            {
-                "<leader>xL",
-                "<cmd>Trouble loclist toggle<cr>",
-                desc = "Location List (Trouble)",
-            },
-            {
-                "<leader>xQ",
-                "<cmd>Trouble qflist toggle<cr>",
-                desc = "Quickfix List (Trouble)",
-            },
-        },
-    }
+	{
+		{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
+		{
+			"stevearc/conform.nvim",
+			config = function()
+				require("mason-tool-installer").setup({
+					ensure_installed = {
+						"prettierd", -- Example formatter
+						"stylua", -- Another formatter
+						"black", -- Python formatter
+					},
+					auto_update = true, -- Auto-update tools when opening Neovim
+				})
+				require("conform").setup({
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						pattern = "*",
+						callback = function(args)
+							require("conform").format({ bufnr = args.buf })
+						end,
+					}),
+					format_on_save = {
+						-- These options will be passed to conform.format()
+						timeout_ms = 500,
+						lsp_fallback = true,
+					},
+					formatters_by_ft = {
+						lua = { "stylua" },
+						-- Conform will run multiple formatters sequentially
+						python = { "isort", "black" },
+						-- Use a sub-list to run only the first available formatter
+						javascript = { { "prettierd", "prettier" } },
+						go = { "goimports", "gofmt" },
+					},
+				})
+			end,
+		},
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		lazy = false,
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {},
+	},
+	{
+		"abecodes/tabout.nvim",
+		opts = {},
+	},
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
 }
--- lua/popo/lsp.lua
+++ lua/popo/lsp.lua
@@ -1,122 +1,137 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            local configs = require("nvim-treesitter.configs")
-            configs.setup({
-                sync_install = true,
-                auto_install = true,
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "typescript", "javascript", "html", "tsx", "go", "rust", "css", "prisma", "cpp" },
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				sync_install = true,
+				auto_install = true,
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"typescript",
+					"javascript",
+					"html",
+					"tsx",
+					"go",
+					"rust",
+					"css",
+					"prisma",
+					"cpp",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "ThePrimeagen/vim-be-good",
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-            "hrsh7th/nvim-cmp",
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
-        },
-        config = function()
-            require("mason").setup({
-                opts = {
-                    ensure_installed = {
-                        "prettierd",
-                        "eslint-lsp",
-                        "tailwindcss-language-server",
-                        "typescript-language-server",
-                    },
-                    automatic_installation = true
-                }
-            })
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "rust_analyzer",
-                    "golangci_lint_ls",
-                    "html",
-                    "marksman",
-                    "cssls",
-                    "clangd", -- C++
-                    "zls",    -- Zig
-                    "pyright",
-                },
-                automatic_installation = true,
-                handlers = {
-                    function(server_name)
-                        local nvim_lsp = require("lspconfig")
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"ThePrimeagen/vim-be-good",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/nvim-cmp",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			require("mason").setup({
+				opts = {
+					ensure_installed = {
+						"prettierd",
+						"eslint-lsp",
+						"tailwindcss-language-server",
+						"typescript-language-server",
+					},
+					automatic_installation = true,
+				},
+			})
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"rust_analyzer",
+					"golangci_lint_ls",
+					"html",
+					"marksman",
+					"cssls",
+					"clangd", -- C++
+					"zls", -- Zig
+					"pyright",
+				},
+				automatic_installation = true,
+				handlers = {
+					function(server_name)
+						local nvim_lsp = require("lspconfig")
 
-                        -- Set up TypeScript server
-                        if server_name == "tsserver" then
-                            nvim_lsp.tsserver.setup({
-                                -- other configuration options...
+						-- Set up TypeScript server
+						if server_name == "ts_ls" then
+							nvim_lsp.ts_ls.setup({
+								-- other configuration options...
 
-                                -- Set the tab width for TypeScript files
-                                settings = {
-                                    typescript = {
-                                        tabSize = 4,
-                                    },
-                                    javascript = {
-                                        tabSize = 4,
-                                    },
-                                },
-                            })
-                        else
-                            -- Set up other language servers
-                            nvim_lsp[server_name].setup({
-                                on_attach = attach,
-                                -- other configuration options...
-                            })
-                        end
-                    end,
-                },
-            })
+								-- Set the tab width for TypeScript files
+								settings = {
+									typescript = {
+										tabSize = 4,
+									},
+									javascript = {
+										tabSize = 4,
+									},
+								},
+							})
+						else
+							-- Set up other language servers
+							nvim_lsp[server_name].setup({
+								on_attach = attach,
+								-- other configuration options...
+							})
+						end
+					end,
+				},
+			})
 
-            -- Auto complete stuff
-            local cmp = require('cmp')
-            local cmp_select = { behavior = cmp.SelectBehavior.Select }
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require('luasnip').lsp_expand(args.body) -- for luasnip.
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                }),
-                sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' },
-                }, {
-                    { name = 'buffer' },
-                })
-            })
+			-- Auto complete stuff
+			local cmp = require("cmp")
+			local cmp_select = { behavior = cmp.SelectBehavior.Select }
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body) -- for luasnip.
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+					["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<C-Space>"] = cmp.mapping.complete(),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
 
-            vim.diagnostic.config({
-                update_in_insert = true,
-                float = {
-                    focusable = false,
-                    style = "minimal",
-                    border = "rounded",
-                    source = "always",
-                    header = "",
-                    prefix = "",
-                },
-            })
-        end
-    }
+			vim.diagnostic.config({
+				update_in_insert = true,
+				float = {
+					focusable = false,
+					style = "minimal",
+					border = "rounded",
+					source = "always",
+					header = "",
+					prefix = "",
+				},
+			})
+		end,
+	},
 }
